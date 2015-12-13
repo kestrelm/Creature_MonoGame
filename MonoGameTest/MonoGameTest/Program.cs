968545c3d3d4799583ba1c15d495ca06a9d51e60
kestrelm <creature@kestrelmoon.com>
2015-12-13 09:40:56 -0800

    Adding new MonoGame Package with Bug FIxes and Sample Visual Studio Sln

---
+++ MonoGameTest/MonoGameTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MonoGameTest
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Sample.Game1())
+                game.Run();
+        }
+    }
+#endif
+}
